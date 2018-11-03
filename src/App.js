0519203f6b2626bb22d4aaa379109459b3d87cf2
Matthew Apple <m.appel998@gmail.com>
2018-11-03 17:51:59 -0500

    Why with the notes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@ import React, { Component } from 'react';
 import './index.css';
 
 export default class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            view: "Home"
+        };
+    }
+
     displayName = App.name
+
     render() {
+        const render;
+
+        if (this.state.view === "Home") {
+            render = <Home />;
+        }
+        else {
+            render = "Try Again";
+        }
+
         return (
             <div className="BodyBackground">
                 <div className="header">Header</div>
                 <div className="wrapper">
-                    <div className="Body">Body</div>
+                    <div className="Body">{render}</div>
                 </div>
                 <div className="Footer">Footer</div>
             </div>
