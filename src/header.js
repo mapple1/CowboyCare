9928dc2653b66e81f2c050409652fb7ea7333b89
Matthew Apple <m.appel998@gmail.com>
2018-11-03 23:33:36 -0500

    css

--- src/header.js
+++ src/header.js
@@ -16,42 +16,18 @@ export default class Header extends Component {
                 </Navbar.Header>
                 <Navbar.Collapse>
                     <Nav>
-                        <NavItem eventKey={1}>
-                    <div className="home">
-                    <div className="Absolute-Center">
-                        <a href="#" className="anchor">Home</a>
-                    </div>
-                            </div>
+                        <NavItem eventKey={1} href="#">Home
                         </NavItem>
-                        <NavItem eventKey={2}>
-                <div className="home">
-                    <div className="Absolute-Center">
-                        <a href="#" className="anchor">Services</a>
-                    </div>
-                            </div>
+                        <NavItem eventKey={2} href="#">Services
                         </NavItem>
-                            <NavItem eventKey={3}>
-                <div className="freq">
-                    <div className="Absolute-Center">
-                        <a href="#" className="anchor">Frequently Asked Questions</a>
-                    </div>
-                            </div>
+                            <NavItem eventKey={3} href="#">Frequently Asked Questions
                         </NavItem>
-                        <div className="home">
-                            <div className="Absolute-Center">
                             <NavDropdown eventKey={4} title="About Us">
                             <menuitem eventKey={4.1}>Our Story</menuitem>
                             <menuitem eventKey={4.2}>Our Care Specialists</menuitem>
                             <menuitem eventKey={4.3}>Something Else</menuitem>
-                                </NavDropdown>
-                        </div>
-                        </div>
-                            <NavItem eventKey={5}>
-                <div className="home">
-                    <div className="Absolute-Center">
-                        <a href="#" className="anchor">Contact Us</a>
-                    </div>
-                                </div>
+                        </NavDropdown>
+                        <NavItem eventKey={5} href="#">Contact Us
                         </NavItem>
                     </Nav>
                 </Navbar.Collapse>
