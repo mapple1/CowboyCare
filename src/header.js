96a1cfb16f926e6f94c194efef6154df77e6fae7
Matthew Apple <m.appel998@gmail.com>
2018-11-03 23:40:35 -0500

    css

--- src/header.js
+++ src/header.js
@@ -8,30 +8,54 @@ export default class Header extends Component {
     render() {
 
         return (
+            <div className="header">
             <Navbar>
                 <Navbar.Header>
                     <Navbar.Brand>
                             <div className="Logo" />
                         </Navbar.Brand>
-                </Navbar.Header>
-                <Navbar.Collapse>
+                    </Navbar.Header>
                     <Nav>
-                        <NavItem eventKey={1} href="#">Home
+                        <NavItem eventKey={1}>
+                    <div className="home">
+                    <div className="Absolute-Center">
+                        <a href="#" className="anchor">Home</a>
+                    </div>
+                            </div>
                         </NavItem>
-                        <NavItem eventKey={2} href="#">Services
+                        <NavItem eventKey={2}>
+                <div className="home">
+                    <div className="Absolute-Center">
+                        <a href="#" className="anchor">Services</a>
+                    </div>
+                            </div>
                         </NavItem>
-                            <NavItem eventKey={3} href="#">Frequently Asked Questions
+                            <NavItem eventKey={3}>
+                <div className="freq">
+                    <div className="Absolute-Center">
+                        <a href="#" className="anchor">Frequently Asked Questions</a>
+                    </div>
+                            </div>
                         </NavItem>
-                            <NavDropdown eventKey={4} title="About Us">
-                            <MenuItem eventKey={4.1}>Our Story</MenuItem>
-                            <MenuItem eventKey={4.2}>Our Care Specialists</MenuItem>
-                            <MenuItem eventKey={4.3}>Something Else</MenuItem>
-                        </NavDropdown>
-                        <NavItem eventKey={5} href="#">Contact Us
+                        <div className="home">
+                            <div className="Absolute-Center">
+                                <NavDropdown eventKey={4} title="About Us">
+                            <menuitem eventKey={4.1}>Our Story</menuitem>
+                            <menuitem eventKey={4.2}>Our Care Specialists</menuitem>
+                            <menuitem eventKey={4.3}>Something Else</menuitem>
+                                </NavDropdown>
+                        </div>
+                        </div>
+                            <NavItem eventKey={5}>
+                <div className="home">
+                    <div className="Absolute-Center">
+                        <a href="#" className="anchor">Contact Us</a>
+                    </div>
+                                </div>
                         </NavItem>
                     </Nav>
-                </Navbar.Collapse>
             </Navbar>
+                        </div>
             );
     }
 }
