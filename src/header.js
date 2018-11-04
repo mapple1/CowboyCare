c99b93d7900b9b4bdf956093de3a032ce8008d5d
Matthew Apple <m.appel998@gmail.com>
2018-11-03 22:48:32 -0500

    css

--- src/header.js
+++ src/header.js
@@ -8,7 +8,7 @@ export default class Header extends Component {
 
         return (
             <div className="header">
-                <div className="Logo" /> <div className="home"><div className="Absolute-Center"><input type="submit" className="anchor">Home</input></div></div>
+                <div className="Logo" /> <div className="home"><div className="Absolute-Center"><a href="#" className="anchor">Home</a></div></div>
             </div>
             );
     }
