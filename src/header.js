5a5f88c5c8b058ca8ce40285a1cd4b5c8f1c1d9f
Matthew Apple <m.appel998@gmail.com>
2018-11-04 10:46:35 -0600

    css

--- src/header.js
+++ src/header.js
@@ -47,7 +47,7 @@ export default class Header extends Component {
                             <div className="Absolute-Center">
                                 <NavItem style={{
                                     marginBlockStart: "0", marginBlockEnd: "0"
-                                }} eventKey={4} className="dropdown" title="About Us" id="nav-dropdown">
+                                }} eventKey={4} className="dropdown" title="About Us" id="nav-dropdown"><a href="#" className="anchor">About Us</a>
                                     <div className="home Absolute-Center menuItem" eventKey={4.1}><a href="#" className="anchor">Our Story</a></div>
                                     <div className="home Absolute-Center menuItem" eventKey={4.2}><a href="#" className="anchor">Our Care Specialists</a></div>
                                     <div className="home Absolute-Center menuItem" eventKey={4.3}><a href="#" className="anchor">Something Else</a></div>
