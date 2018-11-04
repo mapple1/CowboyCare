7750181ca3b321056c833a7bbbc3d71c0de66317
Matthew Apple <m.appel998@gmail.com>
2018-11-04 10:22:13 -0600

    css

--- src/header.js
+++ src/header.js
@@ -9,28 +9,34 @@ export default class Header extends Component {
 
         return (
             <div className="header">
-                <Navbar justified bsStyle="tabs" style={{ display: "flex", flexDirection: "row", padding: "0", listStyle: "none", margin: "none", height: "100px", border: "none" }} staticTop collapseOnSelect>
+                <Navbar justified bsStyle="tabs" style={{ display: "flex", flexDirection: "row", padding: "0", listStyle: "none", margin: "none", marginBlockStart: "0", marginBlockEnd: "0",  height: "100px", border: "none" }} staticTop collapseOnSelect>
                 <Navbar.Header>
                     <Navbar.Brand>
                             <div className="Logo" />
                         </Navbar.Brand>
                     </Navbar.Header>
-                    <Nav justified style={{ display: "flex", flexDirection: "row", listStyle: "none", padding: "0", height: "100px", margin: "none", border: "none" }} bsStyle="tabs" activeKey="1">
-                        <NavItem eventKey={1}>
+                    <Nav justified style={{ display: "flex", flexDirection: "row", listStyle: "none", padding: "0", height: "100px", margin: "none", marginBlockStart: "0", marginBlockEnd: "0",  border: "none" }} bsStyle="tabs" activeKey="1">
+                        <NavItem style={{
+                            marginBlockStart: "0", marginBlockEnd: "0"
+                        }} eventKey = { 1} >
                     <div className="home">
                     <div className="Absolute-Center">
                         <a href="#" className="anchor">Home</a>
                     </div>
                             </div>
                         </NavItem>
-                        <NavItem eventKey={2}>
+                        <NavItem style={{
+                            marginBlockStart: "0", marginBlockEnd: "0"
+                        }} eventKey={2}>
                 <div className="home">
                     <div className="Absolute-Center">
                         <a href="#" className="anchor">Services</a>
                     </div>
                             </div>
                         </NavItem>
-                            <NavItem eventKey={3}>
+                        <NavItem style={{
+                            marginBlockStart: "0", marginBlockEnd: "0"
+                        }} eventKey={3}>
                 <div className="freq">
                     <div className="Absolute-Center">
                         <a href="#" className="anchor">Frequently Asked Questions</a>
@@ -39,14 +45,18 @@ export default class Header extends Component {
                         </NavItem>
                         <div className="home">
                             <div className="Absolute-Center">
-                                <NavDropdown eventKey={4} className="anchor" title="About Us">
+                                <NavDropdown style={{
+                                    marginBlockStart: "0", marginBlockEnd: "0"
+                                }} eventKey={4} className="anchor" title="About Us">
                                     <MenuItem eventKey={4.1}>Our Story</MenuItem>
                                     <MenuItem eventKey={4.2}>Our Care Specialists</MenuItem>
                                     <MenuItem eventKey={4.3}>Something Else</MenuItem>
                                 </NavDropdown>
                             </div>
                         </div>
-                            <NavItem eventKey={5}>
+                        <NavItem style={{
+                            marginBlockStart: "0", marginBlockEnd: "0"
+                        }} eventKey={5}>
                 <div className="home">
                     <div className="Absolute-Center">
                         <a href="#" className="anchor">Contact Us</a>
