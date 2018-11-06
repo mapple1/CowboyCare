95aa3e28f9f13b0329816f8ded9041798442d1df
Matthew Apple <m.appel998@gmail.com>
2018-11-06 07:05:57 -0600

    css

--- src/header.js
+++ src/header.js
@@ -39,52 +39,34 @@ export default class Header extends Component {
                         textDecoration: "none",
                         color: "black"
                     }} eventKey={1} >
-                            <div className="left">
                                 <span className="corner-left" />
-                    <div className="Absolute-Center">
-                        <a href="#" className="anchor">Home</a>
-                    </div>
-                            </div>
+                        Home
                         </NavItem>
                     <NavItem style={{
                         textDecoration: "none",
                         color: "black"
                     }} eventKey={2}>
-                <div className="home">
-                    <div className="Absolute-Center">
-                        <a href="#" className="anchor">Services</a>
-                    </div>
-                            </div>
+                        Services
                         </NavItem>
                     <NavItem style={{
                         textDecoration: "none",
                         color: "black"
-                    }} eventKey={3}>
-                <div className="freq">
-                    <div className="Absolute-Center">
-                        <a href="#" className="anchor">Frequently Asked Questions</a>
-                    </div>
-                            </div>
+                    }} eventKey={3}>Frequently Asked Questions
                         </NavItem>
                     <NavDropdown style={{
                         textDecoration: "none",
                         color: "black"
                     }}
                         title="About Us" eventKey={4} id="basic-nav-dropdown" >
-                            <MenuItem id="menuItem" eventKey={4.1}><a href="#" className="anchor1">Our Story</a></MenuItem >
-                            <MenuItem id="menuItem" eventKey={4.2}><a href="#" className="anchor1">Our Care Specialists</a></MenuItem >
-                            <MenuItem id="menuItem" eventKey={4.3}><a href="#" className="anchor1">Something Else</a></MenuItem >
+                            <MenuItem id="menuItem" eventKey={4.1}>Our Story</MenuItem >
+                            <MenuItem id="menuItem" eventKey={4.2}>Our Care Specialists</MenuItem >
+                            <MenuItem id="menuItem" eventKey={4.3}>Something Else</MenuItem >
                             </NavDropdown>
                     <NavItem style={{
                         textDecoration: "none",
                         color: "black"
                     }} eventKey={5}>
-                        <div className="right">
-                            <span className="corner-right" />
-                    <div className="Absolute-Center">
-                        <a href="#" className="anchor">Contact Us</a>
-                    </div>
-                                </div>
+                            <span className="corner-right" />Contact Us
                         </NavItem>
                     </Nav>
             </Navbar>
