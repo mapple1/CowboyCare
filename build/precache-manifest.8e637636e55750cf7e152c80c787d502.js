cb434f5f7604e24e5f60c857649ef522ea5af79e
Matthew Apple <m.appel998@gmail.com>
2018-11-05 19:12:33 -0600

    css

---
+++ build/precache-manifest.8e637636e55750cf7e152c80c787d502.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d5462bcdf4558aad0d2cbaa6e8b9eee",
+    "url": "/static/media/logo.6d5462bc.png"
+  },
+  {
+    "revision": "a4977bb8cd7540f883a452e9eb32b962",
+    "url": "/static/media/backgroundimg.a4977bb8.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9eb7ede462fc63cd1215",
+    "url": "/static/js/main.9eb7ede4.chunk.js"
+  },
+  {
+    "revision": "a316f6afc8e1b678dcdf",
+    "url": "/static/js/1.a316f6af.chunk.js"
+  },
+  {
+    "revision": "9eb7ede462fc63cd1215",
+    "url": "/static/css/main.5ac8631a.chunk.css"
+  },
+  {
+    "revision": "7af6148418d047c1bca940e3b22021a9",
+    "url": "/index.html"
+  }
+];
