6af1c8cada4cdab3938d7455bf17751f9095ab90
Matthew Apple <m.appel998@gmail.com>
2018-11-05 19:32:07 -0600

    css

--- src/header.js
+++ src/header.js
@@ -14,7 +14,24 @@ export default class Header extends Component {
     render() {
 
         return (
-            <Navbar defaultNavExpanded fluid collapseOnSelect>
+            <Navbar defaultNavExpanded fluid style={{
+                fluid: "true",
+                right: "15%",
+                height: "25%",
+                width: "80%",
+                position: "absolute",
+                alignContent: "center",
+                marginLeft: "auto",
+                marginRight: "auto",
+                display: "flex",
+                flexDirection: "row",
+                padding: "0",
+                listStyle: "none",
+                margin: "none",
+                marginBlockStart: "0",
+                marginBlockEnd: "0",
+                border: "none"
+            }} collapseOnSelect>
                 <Navbar.Header>
                     <Navbar.Brand>
                             <div className="Logo" />
@@ -22,8 +39,10 @@ export default class Header extends Component {
                     <Navbar.Toggle />
                     </Navbar.Header>
                     <Navbar.Collapse>
-                    <Nav justified>
-                        <NavItem eventKey={1} >
+                    <Nav justified style={{ display: "flex", flexDirection: "row", listStyle: "none", padding: "0", margin: "none", marginBlockStart: "0", marginBlockEnd: "0",  border: "none" }} bsStyle="tabs" activeKey="1">
+                        <NavItem style={{
+                            marginBlockStart: "0", marginBlockEnd: "0"
+                        }} eventKey={1} >
                             <div className="home left">
                                 <span className="corner-left" />
                     <div className="Absolute-Center">
@@ -31,7 +50,9 @@ export default class Header extends Component {
                     </div>
                             </div>
                         </NavItem>
-                        <NavItem eventKey={2}>
+                        <NavItem style={{
+                            marginBlockStart: "0", marginBlockEnd: "0"
+                        }} eventKey={2}>
                 <div className="home">
                     <div className="Absolute-Center">
                         <a href="#" className="anchor">Services</a>
@@ -47,13 +68,56 @@ export default class Header extends Component {
                     </div>
                             </div>
                         </NavItem>
-                        <NavDropdown
+                        <NavDropdown style={{
+                            listStyle: "none",
+                            fontSize: "15px",
+                            textDecoration: "none",
+                            color: "black",
+                            textDecorationColor: "translucent",
+                            textAlign: "center",
+                            verticalAlign: "middle",
+                            alignContent: "center",
+                            width: "100px",
+                            height: "50px"
+                        }}
                             title="About Us" eventKey={4} id="basic-nav-dropdown" >
-                            <MenuItem id="menuItem" eventKey={4.1}><a href="#" className="anchor1">Our Story</a></MenuItem >
-                            <MenuItem id="menuItem" eventKey={4.2}><a href="#" className="anchor1">Our Care Specialists</a></MenuItem >
-                            <MenuItem id="menuItem" eventKey={4.3}><a href="#" className="anchor1">Something Else</a></MenuItem >
+                            <MenuItem className="menuItem Absolute-Center" id="menuItem" style={{
+                                listStyle: "none",
+                                textDecoration: "none",
+                                color: "black",
+                                margin: "none",
+                                padding: "none",
+                                textAlign: "center",
+                                verticalAlign: "middle",
+                                alignContent: "center",
+                                maxWidth: "100%"
+                            }} eventKey={4.1}><a href="#" className="anchor1">Our Story</a></MenuItem >
+                            <MenuItem className="menuItem Absolute-Center" id="menuItem" style={{
+                                listStyle: "none",
+                                textDecoration: "none",
+                                color: "black",
+                                margin: "none",
+                                padding: "none",
+                                textAlign: "center",
+                                verticalAlign: "middle",
+                                alignContent: "center",
+                                maxWidth: "100%"
+                            }} eventKey={4.2}><a href="#" className="anchor1">Our Care Specialists</a></MenuItem >
+                            <MenuItem className="menuItem" id="menuItem" style={{
+                                listStyle: "none",
+                                textDecoration: "none",
+                                color: "black",
+                                margin: "none",
+                                padding: "none",
+                                textAlign: "center",
+                                verticalAlign: "middle",
+                                alignContent: "center",
+                                maxWidth: "100%"
+                                }} eventKey={4.3}><a href="#" className="anchor1">Something Else</a></MenuItem >
                             </NavDropdown>
-                        <NavItem eventKey={5}>
+                        <NavItem style={{
+                            marginBlockStart: "0", marginBlockEnd: "0"
+                        }} eventKey={5}>
                 <div className="home">
                     <div className="Absolute-Center">
                         <a href="#" className="anchor">Contact Us</a>
