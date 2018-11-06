320eb70a66e4697a8160e7910015216bd9f0988d
Matthew Apple <m.appel998@gmail.com>
2018-11-06 13:04:38 -0600

    css

--- src/navigation.js
+++ src/navigation.js
@@ -14,7 +14,7 @@ class Navigation extends React.Component {
             <nav id="navigation" className="navbar navbar-expand-lg navbar-light bg-light">
                 <a className="navbar-brand" href="/"><img className="Logo" src="logo.png" /></a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon">Menu</span>
+                    <span className="navbar-toggler-icon"></span>Menu
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
