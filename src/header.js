701495a5f131a53141fa8c4e0b54c694d280cf56
Matthew Apple <m.appel998@gmail.com>
2018-11-06 09:38:00 -0600

    css

--- src/header.js
+++ src/header.js
@@ -25,7 +25,54 @@ export default class Header extends Component {
                     <Navbar.Brand>
                         <img className="Logo" src="logo.png" />
                     </Navbar.Brand>
+                    <Navbar.Toggle className="nav-dropdowntoggle" />
                 </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav id="basic-nav-dropdownCollapse" fluid style={{
+                        listStyle: "none",
+                        textDecoration: "none",
+                        left: "0px",
+                        position: "relative",
+                        width: "100%",
+                        backgroundColor: "saddlebrown"
+                    }} bsStyle="tabs" activeKey="1">
+                        <NavItem id="basic-nav-dropdown" style={{
+                            textDecoration: "none",
+                            color: "black",
+                            borderLeft: "0px"
+                        }} eventKey={1} >
+                            <span className="corner-left" />
+                            Home
+                        </NavItem>
+                        <NavItem id="basic-nav-dropdown" style={{
+                            textDecoration: "none",
+                            color: "black"
+                        }} eventKey={2}>
+                            Services
+                        </NavItem>
+                        <NavItem id="basic-nav-dropdownFreq" style={{
+                            textDecoration: "none",
+                            color: "black"
+                        }} eventKey={3}>Frequently Asked Questions
+                        </NavItem>
+                        <NavDropdown style={{
+                            textDecoration: "none",
+                            color: "black"
+                        }}
+                            title="About Us" eventKey={4} id="basic-nav-dropdown" >
+                            <MenuItem id="menuItem" eventKey={4.1}>Our Story</MenuItem >
+                            <MenuItem id="menuItem" eventKey={4.2}>Our Care Specialists</MenuItem >
+                            <MenuItem id="menuItem" eventKey={4.3}>Something Else</MenuItem >
+                        </NavDropdown>
+                        <NavItem id="basic-nav-dropdown" style={{
+                            textDecoration: "none",
+                            color: "black"
+                        }} eventKey={5}>
+                            Contact Us
+                        </NavItem>
+                        <span className="corner-right" />
+                    </Nav>
+                </Navbar.Collapse>
                 <Nav  id="basic-nav-dropdownnav" fluid style={{
                     listStyle: "none",
                     textDecoration: "none",
