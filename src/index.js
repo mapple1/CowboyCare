0d01cf95b0a17200b4fb2dec5d2849f121038944
Matthew Apple <m.appel998@gmail.com>
2018-11-06 15:33:18 -0600

    css

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import './navi.css';
 import App from './App';
 import "../node_modules/jquery/dist/jquery.min.js";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 import '../node_modules/react-bootstrap/dist/react-bootstrap.min.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
