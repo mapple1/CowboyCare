438a179b5c01b3de7e01a25f463f93ff409810a4
Matthew Apple <m.appel998@gmail.com>
2018-11-07 11:18:33 -0600

    css

--- src/route.js
+++ src/route.js
@@ -11,7 +11,7 @@ export default class rout extends Component {
         const container = () => <div><NewHead /> {this.props.children}</div>;
         const home = () => <Home />;
         const services = () => <Services />;
-        const NotFound = () => <h1> You just got 404'd! </h1>;
+        const NotFound = () => <div className="container"><h1> You just got 404'd! </h1></div>;
         //<NewHead />
         return (
             <div>
