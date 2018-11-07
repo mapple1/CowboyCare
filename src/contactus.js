66e75e2811fb21a8d05fb7d007c91d2916b86e3b
Matthew Apple <m.appel998@gmail.com>
2018-11-07 13:39:27 -0600

    css

--- src/contactus.js
+++ src/contactus.js
@@ -5,13 +5,13 @@ export default class ContactUs extends Component {
     render() {
 
         return (
-            <form action="https://script.google.com/macros/s/AKfycbzVyix1XCoaByGvqxz5DqcUtH_nvb__90okXCtWpA/exec" method="post" enctype="text/plain">
+            <form id="gform" action="https://script.google.com/macros/s/AKfycbzVyix1XCoaByGvqxz5DqcUtH_nvb__90okXCtWpA/exec" method="post" enctype="text/plain">
                 Name:<br/>
                     <input type="text" name="name" /><br />
                         E-mail:<br />
-                            <input type="text" name="mail" /><br />
+                            <input type="text" name="email" /><br />
                                 Comment:<br />
-                                    <input type="text" name="comment" size="50" /><br/><br/>
+                                    <input type="text" name="message" size="50" /><br/><br/>
                                         <input type="submit" value="Send" />
                                             <input type="reset" value="Reset" />
             </form>
