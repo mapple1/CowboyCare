6a35024e4b8fbb27d925f304d6de078fd496ca1d
Matthew Apple <m.appel998@gmail.com>
2018-11-07 14:10:34 -0600

    css

--- src/contactus.js
+++ src/contactus.js
@@ -2,22 +2,16 @@ import React, { Component } from 'react';
 
 export default class ContactUs extends Component {
 
-
-//    <form action="https://formspree.io/your@email.com" method="POST">
-//    <input type="text" name="name">
-//        <input type="email" name="_replyto">
-//            <input type="submit" value="Send">
-//</form>
     render() {
 
         return (
-            <form action="https://formspree.io/ContactCowboy.Care@gmail.com" method="post" enctype="text/plain">
+            <form action="mailto:ContactCowboy.Care@gmail.com" method="post" enctype="text/plain" >
                 Name:<br/>
                     <input type="text" name="name" /><br />
                         E-mail:<br />
-                            <input type="email" name="_replyto" /><br />
+                            <input type="text" name="mail" /><br />
                                 Comment:<br />
-                                    <input type="text" name="message" size="50" /><br/><br/>
+                                    <input type="text" name="comment" size="50" /><br/><br/>
                                         <input type="submit" value="Send" />
                                             <input type="reset" value="Reset" />
             </form>
