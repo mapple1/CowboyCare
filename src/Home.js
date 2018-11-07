7c853e6bda05a448a9933e23839edc1f347c6cbb
Matthew Apple <m.appel998@gmail.com>
2018-11-07 11:39:53 -0600

    css

--- src/Home.js
+++ src/Home.js
@@ -12,7 +12,7 @@ export default class Home extends Component {
                 <img className="horse" src="horse.jpg" />
                 <p style={{ backgroundColor: "white", marginLeft: "10%", marginRight: "10%" }}>
                     Welcome to Cowboy Care of Wyoming! <br />To inquire about our services please
-                    visit <a style={{ textDecoration: "none", color: "black" }} href="https://cowboy-care.netlify.com/#/services"> here</a>
+                    visit <a style={{ textDecoration: "none", color: "saddlebrown" }} href="https://cowboy-care.netlify.com/#/services"> here.</a>
                 </p>
             </div>
             );
--- src/navigation.js
+++ src/navigation.js
@@ -28,7 +28,7 @@ class Navigation extends React.Component {
     render() {
         return (
             <nav id="navigation" className="navbar navbar-expand-xl navbar-light bg-light">
-                <a className="navbar-brand" href="/"><img className="Logo" src="logo.png" /></a>
+                <Link className="navbar-brand" to="/"><img className="Logo" src="logo.png" /></Link>
                 <button id="button-toggle" className="navbar-toggler" onMouseOver={this.onHover.bind(this)} onMouseLeave={this.onLeave.bind(this)} type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded={this.state.toggle}>
                     <span className="navbar-toggler-icon"></span>Menu
                 </button>
