f6d305da3c0c4573d788939d1a2829074c0b5994
Matthew Apple <m.appel998@gmail.com>
2018-11-08 14:08:56 -0600

    css

--- src/navigation.js
+++ src/navigation.js
@@ -30,32 +30,32 @@ class Navigation extends React.Component {
             <nav id="navigation" className="navbar navbar-expand-xl navbar-light bg-light">
                 <Link className="navbar-brand" to="/"><img className="Logo" src="logo.png" /></Link>
                 <button id="button-toggle" className="navbar-toggler" onMouseOver={this.onHover.bind(this)} onMouseLeave={this.onLeave.bind(this)} type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded={this.state.toggle}>
-                    <span className="navbar-toggler-icon"></span><p style={{color: "white"}}>Menu</p>
+                    <span className="navbar-toggler-icon"></span>Menu
                 </button>
 
                 <div className="collapse navbar-collapse" data-pa id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto" id="basic-nav-dropdownnav">
                         <li id="basic-nav-dropdown" className="nav-item active">
-                            <Link className="nav-link" to="/"><p style={{ color: "white" }}>Home</p></Link>
+                            <Link className="nav-link" to="/">Home</Link>
                         </li>
                         <li id="basic-nav-dropdown" className="nav-item active">
-                            <Link className="nav-link" to="/services"><p style={{ color: "white" }}>Services</p></Link>
+                            <Link className="nav-link" to="/services">Services</Link>
                         </li>
                         <li id="basic-nav-dropdownFreq" className="nav-item active">
-                            <a className="nav-link" href="/"><p style={{ color: "white" }}>Frequently Asked Questions</p></a>
+                            <a className="nav-link" href="/">Frequently Asked Questions</a>
                         </li>
                         <li className="nav-item dropdown active" id="basic-nav-dropdown">
                             <a className="nav-link dropdown-toggle" href="/"  role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <p style={{ color: "white" }}>About Us</p>
+                                About Us
                              </a>
                             <div className="dropdown-menu" aria-labelledby="basic-nav-dropdown">
-                                <a id="menuItem" className="dropdown-item" href="/"><p style={{color: "white"}}>Our Story</p></a>
-                                <a id="menuItem" className="dropdown-item" href="/"><p style={{color: "white"}}>Our Care Specialists</p></a>
-                                <a id="menuItem" className="dropdown-item" href="/"><p style={{ color: "white" }}>Something else</p></a>
+                                <a id="menuItem" className="dropdown-item" href="/">Our Story</a>
+                                <a id="menuItem" className="dropdown-item" href="/">Our Care Specialists</a>
+                                <a id="menuItem" className="dropdown-item" href="/">Something else</a>
                             </div>
                         </li>
                         <li id="basic-nav-dropdown" className="nav-item active">
-                            <Link className="nav-link" to="/contact"><p style={{ color: "white" }}>Contact Us</p></Link>
+                            <Link className="nav-link" to="/contact">Contact Us</Link>
                         </li><span className="corner-left" />
                 <span className="corner-right" />
                     </ul>
