a25c1dbf5f01a17a470465c6f5ceac2ea5e32604
Matthew Apple <m.appel998@gmail.com>
2018-11-07 21:02:43 -0600

    css

--- src/Home.js
+++ src/Home.js
@@ -11,7 +11,7 @@ export default class Home extends Component {
             <div className="container">
                 <img className="horse" src="horse.jpg" />
                 <p style={{ backgroundColor: "white", marginLeft: "10%", marginRight: "10%" }}>
-                    Welcome to Cowboy Care of Wyoming! <br />To inquire about our services please
+                    <h3 style={{ color: "#0066cc" }}>Customized Care to Fit Your Needs</h3>
                     visit <a style={{ textDecoration: "none", color: "saddlebrown" }} href="https://cowboy-care.netlify.com/#/services"> here.</a>
                     <br />This is however just a test of what can be done for this site.
                     <br />If you have questions about how things look here please contact me.
