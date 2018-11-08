8902ed6fe81e84dd2f374da4d1c68a9047183a9a
Matthew Apple <m.appel998@gmail.com>
2018-11-07 21:09:42 -0600

    css

--- src/Home.js
+++ src/Home.js
@@ -10,7 +10,8 @@ export default class Home extends Component {
         return (
             <div className="container">
                 <img className="horse" src="horse.jpg" />
-                <p style={{ alignContent: "center", textAlign:"center", backgroundColor: "white", marginLeft: "10%", marginRight: "10%" }}>
+                <p style={{
+                    alignContent: "center", textAlign: "center", padding:"padding: 36px 0px 36px 36px", backgroundColor: "white", marginLeft: "10%", marginRight: "10%" }}>
                     <h3 style={{
                         color: "#0066cc", fontFamily: "Georgia, serif" }}>Customized Care to Fit Your Needs</h3>
                     visit <a style={{ textDecoration: "none", color: "saddlebrown" }} href="https://cowboy-care.netlify.com/#/services"> here.</a>
