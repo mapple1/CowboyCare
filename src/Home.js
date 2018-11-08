34bb6d109b783277cd63a61588ee018cef2cd9a6
Matthew Apple <m.appel998@gmail.com>
2018-11-07 21:24:57 -0600

    css

--- src/Home.js
+++ src/Home.js
@@ -18,12 +18,12 @@ export default class Home extends Component {
                     <h3 style={{
                         color: "#0066cc", fontFamily: "Georgia, serif" }}>Customized Care to Fit Your Needs</h3>
                     At Cowboy Care, our goal is to provide our valued clients with a wonderful alternative to an
-                    <br />assisted living environment, and we accomplish our goal by offering you customized home 
-                    <br />care services. We're committed to quality, because we care. Our caregivers are carefully
-                    <br />selected so that you receive excellent care in your home. Extensive national background 
-                    <br />checks are performed on new employees. We often participate in continuing education 
-                    <br />programs. You can rest assured that only qualified professionals will be serving your needs 
-                    <br />at home. A few of the services we provide to seniors include:<br />
+                    assisted living environment, and we accomplish our goal by offering you customized home 
+                    care services. We're committed to quality, because we care. Our caregivers are carefully
+                    selected so that you receive excellent care in your home. Extensive national background 
+                    checks are performed on new employees. We often participate in continuing education 
+                    programs. You can rest assured that only qualified professionals will be serving your needs 
+                    at home. A few of the services we provide to seniors include:<br />
                     <ul style={{
                         textAlign: "left"
                     }}>
@@ -38,9 +38,9 @@ export default class Home extends Component {
                         <li>Companionship</li>
                         <li>Respite for caregivers</li>
                     </ul>
-                    <br />With the compassionate home care services available to you through Cowboy Care, you can 
-                    <br />easily enjoy a comfortable life inside your home. We'll give you the support you need for all
-                    <br/>your daily activities, so call us today to learn more about our customizable care plans. 
+                    With the compassionate home care services available to you through Cowboy Care, you can 
+                    easily enjoy a comfortable life inside your home. We'll give you the support you need for all
+                    your daily activities, so call us today to learn more about our customizable care plans. 
                 </p>
             </div>
             );
