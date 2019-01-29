b950ec003580c3bcb549aac16d0034690d478e0e
Matthew Apple <m.appel998@gmail.com>
2019-01-28 18:24:17 -0600

    css

--- src/navigation.js
+++ src/navigation.js
@@ -62,7 +62,7 @@ class Navigation extends React.Component {
                                 Contact Us
                             </div>
                             <div className="dropdown-menu" aria-labelledby="basic-nav-dropdown">
-                                <div style={{ verticalAlign: "middle" }}><p id="menuItem" className="dropdown-item" style={{ color: "white", fontWeight: "bold", display: this.state.show }}>Cowboy Care LLC 709 9th Street Wheatland, WY 82201 307-269-0655</p></div>
+                                <div style={{ verticalAlign: "middle" }}><p id="menuItem" className="dropdown-item" style={{ color: "white", fontWeight: "bold" }}>Cowboy Care LLC 709 9th Street Wheatland, WY 82201 307-269-0655</p></div>
                                 <Link id="menuItem" className="dropdown-item" style={{ color: "white", fontWeight: "bold" }} to="/contact">Email Us</Link>
                             </div>
                         </li>
